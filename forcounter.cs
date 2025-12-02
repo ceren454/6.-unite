8aa28ada63e55cd8b46e2201f033b484cde47bfd
ceren454 <cerebtoy@gmail.com>
2025-12-03 00:03:48 +0300

    Add ForCounter class to print numbers 1 to 10

---
+++ forcounter.cs
@@ -0,0 +1,14 @@
+using System;
+
+class ForCounter
+{
+    static void Main()
+    {
+        for (int counter = 1; counter <= 10; counter++)
+        {
+            Console.Write($"{counter}  ");
+        }
+
+        Console.WriteLine();
+    }
+}
